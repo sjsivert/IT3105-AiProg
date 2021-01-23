8a1638c5574b1e5e8007e86165cdc1d9a6ac7682
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-01-23 14:01:05 +0100

    Refactor: GenerateBoard code to classes

--- project1/sim_world/GenerateBoard.py
+++ project1/sim_world/GenerateBoard.py
@@ -1,66 +1,101 @@
-import json
-from Node import Peg
 import networkx as nx
-
 import matplotlib.pyplot as plt
 import os
 import plotly.graph_objects as go
 import plotly.basedatatypes as base
-from Node import Node
 import plotly.graph_objects as go
+from Node import Peg
+from enum import Enum
 
 
-def GenerateTriangle(width: int) -> list[list[int]]:
-    board = []
-    for i in range(1, width+1):
-        board.append(i * [1])
-    return board
+class Boardtype(Enum):
+    triangle = "triangle"
+    diamond = "diamond"
 
 
-def GenerateDiamond(width: int) -> list[list[int]]:
-    board = GenerateTriangle(width)
-    for i in range(width - 1, 0, -1):
-        board.append(i * [1])
-    return board
+class HexBoard():
+    def __init__(self, boardType: Boardtype, boardWidth: int, removeLocations: list[tuple[int]] = []) -> None:
+        if (boardType is Boardtype.triangle):
+            self.board = self.generateTriangle(width=boardWidth)
+            self.board = self.removePegs(removeLocations, self.board)
+        elif (boardType is Boardtype.diamond):
+            self.board = self.generateDiamond(width=boardWidth)
+            self.board = self.removePegs(removeLocations, self.board)
+        else:  # boardType has to be diamond
+            raise ValueError("BoardType has to be triangle or diamond")
+
+    def generateTriangle(self, width: int) -> list[list[int]]:
+        board = []
+        for i in range(1, width+1):
+            board.append(i * [1])
+        return board
+
+    def generateDiamond(self, width: int) -> list[list[int]]:
+        board = self.generateTriangle(width)
+        for i in range(width - 1, 0, -1):
+            board.append(i * [1])
+        return board
+
+    def removePegs(self, removeLocations: list[tuple[int]], board: list[list[int]]) -> list[list[int]]:
+        for removeLocation in removeLocations:
+            board[removeLocation[0]][removeLocation[1]] = 0
+        return board
+
+    def __repr__(self):
+        return self.board
+
+    def __str__(self):
+        return str(self.board)
+
+    def __iter__(self):
+        return self.board
 
 
-def removePegs(removeLocations: list[tuple[int]], board: list[list[int]]) -> list[list[int]]:
-    for removeLocation in removeLocations:
-        board[removeLocation[0]][removeLocation[1]] = 0
-    return board
+class BoardState():
+    def __init__(self, hexBoard: HexBoard) -> None:
+        self.state = self.boardToNodes(hexBoard=hexBoard)
 
+    def boardToNodes(self, hexBoard: HexBoard) -> list[list[Peg]]:
+        board = hexBoard.board
+        pegList = []
+        for x in range(len(board)):
+            layerList = []
+            for y in range(len(board[x])):
+                layerList.append(Peg((x, y), board[x][y]))
+                if(y > 0):
+                    layerList[y].addBiDirectionalNeighbour(
+                        layerList[y - 1], "0,-1")
+                if(x > 0):
+                    layerGrowthFactor = len(board[x - 1]) - len(board[x])
+                    secoundParentConnection = y + layerGrowthFactor  # other node above you
+                    if(y < len(pegList[x-1])):
+                        # add yourself as connection to node above you
+                        pegList[x-1][y].addBiDirectionalNeighbour(
+                            layerList[y], "-1," + str(-layerGrowthFactor))
+                    if(secoundParentConnection >= 0 and secoundParentConnection < len(pegList[x - 1])):
+                        # add yourself as connection to other node above you
+                        pegList[x-1][secoundParentConnection].addBiDirectionalNeighbour(
+                            layerList[y], "-1," + str(layerGrowthFactor))
 
-def BoardToNodes(board: list[list[int]]) -> list[list[Peg]]:
-    pegList = []
-    for x in range(len(board)):
-        layerList = []
-        for y in range(len(board[x])):
-            layerList.append(Peg((x, y), board[x][y]))
-            if(y > 0):
-                layerList[y].addBiDirectionalNeighbour(
-                    layerList[y - 1], "0,-1")
-            if(x > 0):
-                layerGrowthFactor = len(board[x - 1]) - len(board[x])
-                secoundParentConnection = y + layerGrowthFactor  # other node above you
-                if(y < len(pegList[x-1])):
-                    # add yourself as connection to node above you
-                    pegList[x-1][y].addBiDirectionalNeighbour(
-                        layerList[y], "-1," + str(-layerGrowthFactor))
-                if(secoundParentConnection >= 0 and secoundParentConnection < len(pegList[x - 1])):
-                    # add yourself as connection to other node above you
-                    pegList[x-1][secoundParentConnection].addBiDirectionalNeighbour(
-                        layerList[y], "-1," + str(layerGrowthFactor))
+            pegList.append(layerList)
+        return pegList
 
-        pegList.append(layerList)
-    return pegList
+    def __repr__(self):
+        return self.state
+
+    def __str__(self):
+        return str(self.state)
 
 
 if __name__ == "__main__":
-    print(GenerateTriangle(4))
-    print(GenerateDiamond(4))
-    b = GenerateTriangle(4)
-    print(removePegs([(0, 0), (1, 0)], b))
-    pegList = BoardToNodes(b)
+    print(HexBoard(Boardtype["triangle"], 4))
+    board = HexBoard(Boardtype["diamond"], 4)
+    state = BoardState(board)
+    print(state)
+    # print(GenerateDiamond(4))
+    # b = GenerateTriangle(4)
+    # print(removePegs([(0, 0), (1, 0)], b))
+    # pegList = BoardToNodes(b)
 
     """def plotTree(node: Peg, G, drawnList: list) -> None:
         print(node.location)
