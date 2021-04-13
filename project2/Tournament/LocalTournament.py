7defb3654be220a2859f6f0d3c57caa1779a4435
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-04-13 21:12:59 +0200

    Fix TOPP load agents automaticly

--- project2/Tournament/LocalTournament.py
+++ project2/Tournament/LocalTournament.py
@@ -10,7 +10,7 @@ class LocalTournament:
     def __init__(self, agents: List, roundRobin:bool, simWorldTemplate, agentNames: Dict):
         with open('project2/parameters.json') as f:
             parameters = json.load(f)
-        numCachedToppPreparations = 0 #parameters['anet_n_cached_topp_preparations']
+        numCachedToppPreparations = parameters['anet_n_cached_topp_preparations']
         numToppGamesToPlay = parameters['anet_n_of_topp_games_to_be_played']
         saveInterval = parameters['save_interval']
         fileNamePrefix = parameters['file_name']
